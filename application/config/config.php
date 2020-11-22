a81ef9fe17a43c0fdd2cdea0567cc8717a8bd149
memetfeb <memet.febriyanto@gmail.com>
2020-11-22 09:05:27 +0700

    tes gitignore 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://apotik.sulameto.com/';
+$config['base_url'] = 'https://http://localhost/apriori_apotika/';
 
 /*
 |--------------------------------------------------------------------------
