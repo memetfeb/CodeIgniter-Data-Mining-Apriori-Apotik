ed78ec673b4755b6c40f5e930aaa84d3c7fdad07
mailliw17 <williamwirahardi@gmail.com>
2020-11-25 14:55:29 +0700

    proses apriori masih error

--- application/controllers/admin/Data_transaksi.php
+++ application/controllers/admin/Data_transaksi.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Data_transaksi extends CI_Controller
 {
@@ -41,23 +41,21 @@ class Data_transaksi extends CI_Controller
     }
 
     // file upload functionality
-    public function importTransaksi() 
+    public function importTransaksi()
     {
         $data = array();
         // Load form validation library
         $this->load->library('form_validation');
         $this->form_validation->set_rules('fileURL', 'Upload File', 'callback_checkFileValidation');
         // If file uploaded
-        if(!empty($_FILES['fileURL']['name'])) 
-        { 
+        if (!empty($_FILES['fileURL']['name'])) {
             // get file extension
             $extension = pathinfo($_FILES['fileURL']['name'], PATHINFO_EXTENSION);
-            if($extension == 'csv')
-            {
+            if ($extension == 'csv') {
                 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
-            }elseif($extension == 'xlsx') {
+            } elseif ($extension == 'xlsx') {
                 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-            }else{
+            } else {
                 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
             }
             // file path
@@ -69,52 +67,43 @@ class Data_transaksi extends CI_Controller
             $createArray = array('id_transaksi', 'transaction_date', 'produk', 'total');
             $makeArray = array('id_transaksi' => 'id_transaksi', 'transaction_date' => 'transaction_date', 'produk' => 'produk', 'total' => 'total');
             $SheetDataKey = array();
-            foreach ($allDataInSheet as $dataInSheet) 
-            {
-                foreach ($dataInSheet as $key => $value) 
-                {
-                    if (in_array(trim($value), $createArray)) 
-                    {
+            foreach ($allDataInSheet as $dataInSheet) {
+                foreach ($dataInSheet as $key => $value) {
+                    if (in_array(trim($value), $createArray)) {
                         $value = preg_replace('/\s+/', '', $value);
                         $SheetDataKey[trim($value)] = $key;
-                    } 
+                    }
                 }
             }
             $dataDiff = array_diff_key($makeArray, $SheetDataKey);
-            if (empty($dataDiff)) 
-            {
+            if (empty($dataDiff)) {
                 $flag = 1;
             }
             // match excel sheet column
-            if ($flag == 1) 
-            {
-                for ($i = 2; $i <= $arrayCount; $i++) 
-                {
+            if ($flag == 1) {
+                for ($i = 2; $i <= $arrayCount; $i++) {
                     $addresses = array();
                     $no = '';
                     $id_transaksi = $SheetDataKey['id_transaksi'];
                     $transaction_date = $SheetDataKey['transaction_date'];
                     $produk = $SheetDataKey['produk'];
                     $total = $SheetDataKey['total'];
-                                        
+
                     $id_transaksi = filter_var(trim($allDataInSheet[$i][$id_transaksi]), FILTER_SANITIZE_STRING);
                     $transaction_date = filter_var(trim($allDataInSheet[$i][$transaction_date]), FILTER_SANITIZE_STRING);
                     $produk = filter_var(trim($allDataInSheet[$i][$produk]), FILTER_SANITIZE_STRING);
                     $total = filter_var(trim($allDataInSheet[$i][$total]), FILTER_SANITIZE_STRING);
-                    
+
                     $fetchData[] = array('id' => $no, 'id_transaksi' => $id_transaksi, 'transaction_date' => $transaction_date, 'produk' => $produk, 'total' => $total);
                 }
                 $data['transaksi'] = $fetchData;
                 $this->admin_model->setBatchImport($fetchData);
                 $this->admin_model->importData();
-            }else{
+            } else {
                 echo "Please import correct file, did not match excel sheet column";
             }
             $this->session->set_flashdata('success', 'Berhasil ditambah');
             redirect(site_url('admin/data_transaksi'));
-        }            
-        
+        }
     }
-
-
 }
---
+++ application/views/admin/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Configuration Database
+ */
+return array(
+    'host' => "localhost",
+    'username' => "root",
+    'password' => "root",
+    'dbname' => "sulameto_apotik",
+    // 'port' => "", // sementara menggunakan port bawaan
+);
